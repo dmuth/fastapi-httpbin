31b41bb8311d2ed1d8465918736f3f65798b2e73
Douglas Muth <dmuth@dmuth.org>
2022-08-09 21:09:40 -0400

    Added get()

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
 
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Header, Request
 from pydantic import BaseModel
 
 
 app = FastAPI()
+
+
+#
+# TODO:
+#
+# X HTTP Methods
+# - Push to GitHub private repo
+# 2 Status Codes
+# 3 Figure out how to group endpoints in docs (tags?)
+# 4 Make main page go to docs
+# - Write some unit tests
+# - Deploy to Deta
+# - Next batch of docs
+
+@app.get("/get")
+async def get(request: Request):
+
+	retval = {}
+
+	retval["args"] = request.query_params
+	retval["headers"] = request.headers
+	retval["source"] = {
+		"ip": request.client[0],
+		"port": request.client[1]
+		}
+	retval["url"] = request.url
+
+	return(retval)
+
 
 class Item(BaseModel):
     name: str

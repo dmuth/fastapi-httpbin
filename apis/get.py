6010d410966805e62c8443790e92f8263032c5e0
Douglas Muth <dmuth@dmuth.org>
2022-08-11 21:03:36 -0400

    Refactoring

--- apis/get.py
+++ apis/get.py
@@ -8,8 +8,10 @@ from fastapi import FastAPI, Header, Request
 router = APIRouter()
 
 
-@router.get("/get")
-async def get(request: Request):
+#
+# Our core function to return the same data for each request.
+#
+def core(request: Request):
 
 	retval = {}
 
@@ -21,73 +23,37 @@ async def get(request: Request):
 		}
 	retval["url"] = request.url
 
+	return(retval)
+
+
+@router.get("/get")
+async def get(request: Request):
+	retval = core(request)
 	return(retval)
 
 
 @router.delete("/delete")
 async def delete(request: Request):
-
-	retval = {}
-
-	retval["args"] = request.query_params
-	retval["headers"] = request.headers
-	retval["source"] = {
-		"ip": request.client[0],
-		"port": request.client[1]
-		}
-	retval["url"] = request.url
-
+	retval = core(request)
 	return(retval)
 
 
 @router.patch("/patch")
 async def patch(request: Request):
-
-	retval = {}
-
-	retval["args"] = request.query_params
-	retval["headers"] = request.headers
-	retval["source"] = {
-		"ip": request.client[0],
-		"port": request.client[1]
-		}
-	retval["url"] = request.url
-
+	retval = core(request)
 	return(retval)
 
 
 @router.post("/post")
 async def post(request: Request):
-
-	retval = {}
-
-	retval["args"] = request.query_params
-	retval["headers"] = request.headers
-	retval["source"] = {
-		"ip": request.client[0],
-		"port": request.client[1]
-		}
-	retval["url"] = request.url
-
+	retval = core(request)
 	return(retval)
 
 
 @router.put("/put")
 async def put(request: Request):
-
-	retval = {}
-
-	retval["args"] = request.query_params
-	retval["headers"] = request.headers
-	retval["source"] = {
-		"ip": request.client[0],
-		"port": request.client[1]
-		}
-	retval["url"] = request.url
-
+	retval = core(request)
 	return(retval)
-
-
 
 
 

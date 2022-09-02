492c9c8e10ba95a16f4e9a54d7a0382f0536e769
Douglas Muth <dmuth@dmuth.org>
2022-09-01 20:32:26 -0400

    Moved some FastAPI settings into lib/fastapi.py

--- main.py
+++ main.py
@@ -17,74 +17,12 @@ from apis import cookies
 from apis import images
 from apis import dynamic
 
-
-tags_metadata = [
-    {
-        "name": "HTTP Methods",
-        "description": "Testing different HTTP verbs."
-    },
-    {
-        "name": "Request Inspection",
-        "description": "Inspect the request data."
-    },
-    {
-        "name": "Responses",
-        "description": "Inspect response data like caching and headers."
-    },
-    {
-        "name": "Response Formats",
-        "description": "Returns responses in different formats."
-    },
-    {
-        "name": "Status Codes",
-        "description": "Generate responses with specified status codes."
-    },
-    {
-        "name": "Redirects",
-        "description": "Return different redirects."
-    },
-    {
-        "name": "Anything",
-        "description": "Return anything that is passed in on the request."
-    },
-    {
-        "name": "Cookies",
-        "description": "Create, read, and delete cookies."
-    },
-    {
-        "name": "Images",
-        "description": "Return different image formats."
-    },
-    {
-        "name": "Dynamic Data",
-        "description": "Generate random and dynamic data."
-    },
-    ]
-
-description = """
-HTTP Endpoints for easy testing of your app.
-
-Built with the <a href="https://fastapi.tiangolo.com/">FastAPI framework</a>, 
-this is heavily based on the original <a href="https://httpbin.org/">Httpbin</a> website.
-
-<a href="/about">About this project</a> - 
-<a href="/roadmap">Development Roadmap</a> -
-<a href="https://github.com/dmuth/fastapi-httpbin">GitHub repo</a>
-
-Run locally in Docker: <tt><b>docker run -p 80:80 dmuth1/fastapi-httpbin</b></tt>
-
-
-<a href="https://httpbin.dmuth.org/">Main Site</a> - Mirrors: 
-<a href="https://fly.httpbin.dmuth.org/">Fly</a>
-<a href="https://railway.httpbin.dmuth.org/">Railway</a>
-<a href="https://render.httpbin.dmuth.org/">Render</a>
-
-"""
+from lib.fastapi import tags_metadata, description
 
 app = FastAPI(docs_url = "/", redoc_url = None,
     title = "FastAPI Httpbin",
     description = description,
-    version = "0.0.4",
+    version = "0.0.5",
     swagger_ui_parameters = {"docExpansion":"none"},
     openapi_tags = tags_metadata
     )

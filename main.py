f3d1131ad574d24778eb4acec24608c9119cb2ff
Douglas Muth <dmuth@dmuth.org>
2022-09-05 18:40:22 -0400

    Version bump.  Turns out that GitHub actions don't play nice with --force-with-lease.  Lesson learned!

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ from lib.fastapi import tags_metadata, description
 app = FastAPI(docs_url = "/", redoc_url = None,
     title = "FastAPI Httpbin",
     description = description,
-    version = "0.0.6",
+    version = "0.0.7.1",
     swagger_ui_parameters = {"docExpansion":"none"},
     openapi_tags = tags_metadata
     )

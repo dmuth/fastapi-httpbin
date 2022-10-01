c9b8f052c7a4302f8036d33b373c6775ff455be1
Douglas Muth <dmuth@dmuth.org>
2022-10-01 18:52:09 -0400

    Added logo and updated social media tags.

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ from lib.fastapi import tags_metadata, description
 app = FastAPI(docs_url = "/", redoc_url = None,
     title = "FastAPI Httpbin",
     description = description,
-    version = "0.0.12",
+    version = "0.0.13",
     swagger_ui_parameters = {"docExpansion":"none"},
     openapi_tags = tags_metadata
     )

ada75479f75d2e8ea6efc1be987f7d0ba1a58ec2
Douglas Muth <dmuth@dmuth.org>
2022-10-01 21:17:57 -0400

    Tweaked text.

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ from lib.fastapi import tags_metadata, description
 app = FastAPI(docs_url = "/", redoc_url = None,
     title = "FastAPI Httpbin",
     description = description,
-    version = "0.0.18",
+    version = "0.0.19",
     swagger_ui_parameters = {"docExpansion":"none"},
     openapi_tags = tags_metadata
     )

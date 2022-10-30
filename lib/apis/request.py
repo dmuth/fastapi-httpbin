da281a0f8d624012167c383de105e0b02cc687c5
Douglas Muth <dmuth@dmuth.org>
2022-10-30 19:24:52 -0400

    Added /ip/v4 and /ip/v6 endpoints.

--- lib/apis/request.py
+++ lib/apis/request.py
@@ -2,7 +2,7 @@
 # All requests
 #
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi import FastAPI, Header, Request
 
 from . import PrettyJSONResponse
@@ -36,21 +36,88 @@ async def get(request: Request):
     return(retval)
 
 
+#
+# Get our IP address
+#
+def get_ip(headers, client):
+
+    if "fly-client-ip" in headers:
+        retval = headers["fly-client-ip"]
+
+    elif "x-forwarded-for" in headers:
+        retval = headers["x-forwarded-for"]
+
+    else:
+        retval = client[0]
+
+    return(retval)
+
+
 @router.get("/ip", summary = "Return the user's IP address.", 
     response_class=PrettyJSONResponse)
 async def ip(request: Request):
+
     retval = {}
 
-    if "fly-client-ip" in request.headers:
-        retval["ip"] = request.headers["fly-client-ip"]
+    ip = get_ip(request.headers, request.client)
+    retval["ip"] = ip
 
-    elif "x-forwarded-for" in request.headers:
-        retval["ip"] = request.headers["x-forwarded-for"]
-
-    else:
-        retval["ip"] = request.client[0]
+    retval["message"] = "If you're looking for v4 or v6 specific endpoints, try /ip/v4 or /ip/v6."
 
     return(retval)
+
+
+@router.get("/ip/v4", summary = "Return the user's IPv4 address (if present).", 
+    responses={
+        200: {
+            "description": "Return the user's IPv4 address, if present.",
+        },
+        422: {
+            "description": "Returned if user came in over IPv6",
+        },
+    },
+    response_class=PrettyJSONResponse)
+async def ipv4(request: Request):
+
+    retval = {}
+    retval["ip"] = ""
+
+    ip = get_ip(request.headers, request.client)
+
+    if ":" not in ip:
+        retval["ip"] = ip
+    else:
+        retval["message"] = "This does not appear to be a request made over IPv4"
+        raise HTTPException(status_code = 422, detail = retval)
+
+    return(retval)
+
+
+@router.get("/ip/v6", summary = "Return the user's IPv6 address (if present).", 
+    responses={
+        200: {
+            "description": "Return the user's IPv6 address, if present.",
+        },
+        422: {
+            "description": "Returned if user came in over IPv4",
+        },
+    },
+    response_class=PrettyJSONResponse)
+async def ipv6(request: Request):
+
+    retval = {}
+    retval["ip"] = ""
+
+    ip = get_ip(request.headers, request.client)
+
+    if ":" in ip:
+        retval["ip"] = ip
+    else:
+        retval["message"] = "This does not appear to be a request made over IPv6"
+        raise HTTPException(status_code = 422, detail = retval)
+
+    return(retval)
+
 
 
 @router.get("/user-agent", summary = "Request the User Agent.",
--- tests/test_request.py
+++ tests/test_request.py
@@ -7,23 +7,85 @@ from main import app
 
 client = TestClient(app)
 
+
+ipv4_1 = "1.2.3.4"
+ipv4_2 = "1.2.3.5"
+ipv6_1 = "2001:db8:0000:1:2:3:4:5"
+ipv6_2 = "2001:db8::5"
+
+
 def test_headers():
     response = client.get("/headers")
     assert response.status_code == 200
     assert response.json()["host"] == "testserver"
+
 
 def test_ip():
     response = client.get("/ip")
     assert response.status_code == 200
     assert response.json()["ip"] == "testclient"
 
-    response = client.get("/ip", headers = {"x-forwarded-for": "1.2.3.4"})
+    response = client.get("/ip", headers = {"x-forwarded-for": ipv4_1})
     assert response.status_code == 200
-    assert response.json()["ip"] == "1.2.3.4"
+    assert response.json()["ip"] == ipv4_1
 
-    response = client.get("/ip", headers = {"x-forwarded-for": "1.2.3.4", "fly-client-ip": "1.2.3.5"})
+    response = client.get("/ip", headers = {"x-forwarded-for": ipv6_1})
     assert response.status_code == 200
-    assert response.json()["ip"] == "1.2.3.5"
+    assert response.json()["ip"] == ipv6_1
+
+    response = client.get("/ip", headers = {"x-forwarded-for": ipv4_1, "fly-client-ip": ipv4_2})
+    assert response.status_code == 200
+    assert response.json()["ip"] == ipv4_2
+
+    response = client.get("/ip", headers = {"x-forwarded-for": ipv6_1, "fly-client-ip": ipv6_2})
+    assert response.status_code == 200
+    assert response.json()["ip"] == ipv6_2
+
+
+def test_ipv4():
+    response = client.get("/ip/v4")
+    assert response.status_code == 200
+    assert response.json()["ip"] == "testclient"
+
+    response = client.get("/ip/v4", headers = {"x-forwarded-for": ipv4_1})
+    assert response.status_code == 200
+    assert response.json()["ip"] == ipv4_1
+
+    response = client.get("/ip/v4", headers = {"x-forwarded-for": ipv4_1, "fly-client-ip": ipv4_2})
+    assert response.status_code == 200
+    assert response.json()["ip"] == ipv4_2
+
+    response = client.get("/ip/v4", headers = {"x-forwarded-for": ipv6_1})
+    assert response.status_code == 422
+    assert "IPv4" in response.json()["detail"]["message"]
+
+    response = client.get("/ip/v4", headers = {"x-forwarded-for": ipv6_1, "fly-client-ip": ipv6_2})
+    assert response.status_code == 422
+    assert "IPv4" in response.json()["detail"]["message"]
+
+
+def test_ipv6():
+
+    response = client.get("/ip/v6")
+    assert response.status_code == 422
+    assert "IPv6" in response.json()["detail"]["message"]
+
+    response = client.get("/ip/v6", headers = {"x-forwarded-for": ipv6_1})
+    assert response.status_code == 200
+    assert response.json()["ip"] == ipv6_1
+
+    response = client.get("/ip/v6", headers = {"x-forwarded-for": ipv6_1, "fly-client-ip": ipv6_2})
+    assert response.status_code == 200
+    assert response.json()["ip"] == ipv6_2
+
+    response = client.get("/ip/v6", headers = {"x-forwarded-for": ipv4_1})
+    assert response.status_code == 422
+    assert "IPv6" in response.json()["detail"]["message"]
+
+    response = client.get("/ip/v6", headers = {"x-forwarded-for": ipv4_1, "fly-client-ip": ipv4_2})
+    assert response.status_code == 422
+    assert "IPv6" in response.json()["detail"]["message"]
+
 
 def test_user_agent():
     response = client.get("/user-agent")

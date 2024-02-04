e80cc0162e63fdfc0083e67d54d6549904f794d6
caroltyk <carol@tyk.io>
2024-02-04 15:48:09 +0000

    github action

--- bin/docker-push.sh
+++ bin/docker-push.sh
@@ -9,7 +9,15 @@ set -e
 # Change to the parent directory of this script
 pushd $(dirname $0)/.. > /dev/null
 
-docker tag fastapi-httpbin dmuth1/fastapi-httpbin
-docker push dmuth1/fastapi-httpbin
+if test "$1"
+then
+    echo "# Publish image with tag: $1"
+    
+    docker tag fastapi-httpbin caroltyk/fastapi-httpbin:$1
 
+    docker push caroltyk/fastapi-httpbin:$1
 
+    oras push registry-1.docker.io/caroltyk/apis:fastapi-httpbin-$1 openapi.json:application/json
+else
+    echo "# Specify an image tag"
+fi

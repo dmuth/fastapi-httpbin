fc850fa9dc3d4bf44aaec0e93923339c4df7e78d
caroltyk <carol@tyk.io>
2024-05-15 19:52:03 +0100

    update deployapi.sh script

--- bin/deployapi.sh
+++ bin/deployapi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Deploy out app to both Fly and build and push a Docker container.
+# Deploy app to build and push a Docker container.
 #
 
 # Errors are fatal
@@ -16,6 +16,29 @@ echo "# "
 python3 extract-openapi.py main:app --out openapi.json
 
 echo "# "
-echo "# Pushing OpenAPI document to Docker Hub..."
+echo "# Converting to Tyk OAS API Definition..."
 echo "# "
-oras push registry-1.docker.io/caroltyk/apis:fastapi-httpbin-$1 openapi.json:application/json
+body=$(<openapi.json)
+api_id=$(curl --location --request POST 'http://tyk-dashboard.org/api/apis/oas/import' \
+  --header 'Authorization: e2f234ac32ab443e78962ef941898722' \
+  --header 'Content-Type: text/plain' \
+  --data-raw $body | jq -r '.ID')
+sleep 1
+
+echo "# "
+echo "# Temporary API ID $api_id ..."
+echo "# "
+sleep 1
+
+echo "# "
+echo "# Retrieve API Definition JSON..."
+echo "# "
+curl --location --request GET 'http://tyk-dashboard.org/api/apis/oas/$api_id' \
+  --header 'Authorization: e2f234ac32ab443e78962ef941898722' \
+  --header 'Content-Type: text/plain' > apidefinition.json
+sleep 1
+
+echo "# "
+echo "# Pushing API Definition to Docker Hub..."
+echo "# "
+oras push registry-1.docker.io/caroltyk/apis:fastapi-httpbin-$1 apidefinition.json:application/json
